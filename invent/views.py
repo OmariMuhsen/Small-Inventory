9d075112ba77146cb01a879ed50e0f368410da93
MuhsenOmari <muhsenomari44@gmail.com>
2025-04-29 09:44:11 +0430

    Views Added

--- invent/views.py
+++ invent/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# backend/inventory/views.py
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Item, Person, Category
+from .serializers import ItemSerializer, PersonSerializer, CategorySerializer
 
-# Create your views here.
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    permission_classes = [IsAuthenticated]
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'assigned_to']
+    search_fields = ['name', 'description']
